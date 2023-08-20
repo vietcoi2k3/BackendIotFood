2af94313475a6881a34eac90b5fefff72c3f1cab
viet <viet1234561231@gmail.com>
2023-08-20 16:55:15 +0700

    aaa

--- src/main/java/com/apec/pos/Security/WebSocketSecurityConfig.java
+++ src/main/java/com/apec/pos/Security/WebSocketSecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
-@Configuration
-@EnableWebSocketSecurity 
 public class WebSocketSecurityConfig {
 
 	   @Bean
