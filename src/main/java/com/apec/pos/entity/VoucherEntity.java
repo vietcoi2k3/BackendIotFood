fdaa0101b62fa8f37abdf9232e2d413d8e637a2f
viet <viet1234561231@gmail.com>
2023-10-03 21:11:56 +0700

    a

--- src/main/java/com/apec/pos/entity/VoucherEntity.java
+++ src/main/java/com/apec/pos/entity/VoucherEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -13,9 +14,10 @@ import java.util.Date;
 @AllArgsConstructor
 @Data
 public class VoucherEntity extends BaseEntity{
-    private String discount;
+    private int discount;
     private LocalDate expired;
     private String detail;
     private String idVoucher;
     private int quantity;
+    private String title;
 }
