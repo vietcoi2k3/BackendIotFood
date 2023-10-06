b05909ad37f9e4fc8080411adfbf00d4481c0d90
viet <viet1234561231@gmail.com>
2023-10-06 17:17:42 +0700

    accept product

--- src/main/java/com/apec/pos/unitl/SocketConfig.java
+++ src/main/java/com/apec/pos/unitl/SocketConfig.java
@@ -37,7 +37,7 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     @CrossOrigin
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-iotfood").setAllowedOrigins("http://127.0.0.1:5500","https://thangdev.online","http://127.0.0.1:5500").withSockJS();
+        registry.addEndpoint("/ws-iotfood").setAllowedOrigins("http://127.0.0.1:5500","https://thangdev.online","http://127.0.0.1:5500","https://food.labtlu.shop").withSockJS();
     }
 
 //    @Override
